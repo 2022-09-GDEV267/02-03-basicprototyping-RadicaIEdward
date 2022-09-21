bc3a957d759d454c93edf9ad3068a8c737a39b19
RadicaIEdward <edward.o.galindez@gmail.com>
2022-09-21 19:31:42 -0400

    Create halo effect for Slingshot

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -3,15 +3,24 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject launchPoint;
+
+    private void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+    }
+   
     void OnMouseEnter()
     {
-        print("Slingshot:OnMouseEnter()");
+        //print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true);
     }
 
-    // Update is called once per frame
     void OnMouseExit()
     {
-        print("Slingshot:OnMouseExit()");
+        //print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);
     }
 }
